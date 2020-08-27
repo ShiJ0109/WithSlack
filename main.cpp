1f65602b7165ed645944a7e6239b53267261614f
Shi <c30109@gmail.com>
2020-08-28 01:11:25 +0800

    comment something.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[])
     file.open(QFile::ReadOnly);
     app.setStyleSheet(file.readAll());
 
-    CandyDialog dialog;
-    dialog.show();
+    //CandyDialog dialog;
+    //dialog.show();
 
     return app.exec();
 }
